ee1e86dca6c23bac4a4b59b7b8b4155a92745dc0
dimdimah <dimahluthfi15@gmail.com>
2024-06-20 23:45:17 +0700

    feat: add page for user & detail schedule user

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3221'
+    }
+  }
+});
