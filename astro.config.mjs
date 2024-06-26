76147f3506d9bdae65a0325102701f2e65727ab5
dimdimah <dimahluthfi15@gmail.com>
2024-06-26 11:07:35 +0700

    style: slicing ui [id].astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,31 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   server: {
     proxy: {
-      '/api': 'http://localhost:3221'
+      '/api': ''
     }
   },
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  experimental: {
+    env: {
+      schema: {
+        API_PORT: envField.number({
+          context: 'server',
+          access: 'secret',
+          default: 7000
+        }),
+        PUBLIC_API_URL: envField.string({
+          context: 'server',
+          access: 'public',
+          default: import.meta.env.BASE_URL
+        }),
+      }
+    }
+  }
 });
